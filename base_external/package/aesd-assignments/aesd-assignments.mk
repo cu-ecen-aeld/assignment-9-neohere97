df4e6235f9cdacb754fd522816f7660c4672f47c
Chinmay Shalawadi <shalawadi.chinmay@gmail.com>
2022-10-23 18:28:39 -0600

    success

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '3bace8c78f05804efc72325d35d497de79ac8226'
+AESD_ASSIGNMENTS_VERSION = '361c341a345c21aae5887767b92fe78ec4a6aed2'
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
